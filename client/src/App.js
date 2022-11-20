b3c879fa31a14d4ff13918e200fab687bac67abf
EllisTse <devopsaron316@EllisPC>
2022-11-20 18:04:04 +0800

    d

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React with docker 
+            dsadasdasdasdasdasdar dsadasdasdasd
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
